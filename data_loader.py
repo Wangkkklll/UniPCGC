2cb02a42eb4bb95b2b8f5a48ed4a227af4c1df31
Kangli Wang <71534709+Wangkkklll@users.noreply.github.com>
2025-12-08 09:40:04 +0800

    fixed the bug of kdtree_partition(

--- data_loader.py
+++ data_loader.py
@@ -161,6 +161,32 @@ def read_coords(filedir):
 
 
 
+def kdtree_partition(points, max_num, n_parts=None):
+    parts = []
+    if n_parts is not None: max_num = len(points)/n_parts + 2
+    class KD_node:  
+        def __init__(self, point=None, LL = None, RR = None):  
+            self.point = point  
+            self.left = LL  
+            self.right = RR
+    def createKDTree(root, data):
+        if len(data) <= max_num:
+            parts.append(data)
+            return
+        variances = (np.var(data[:, 0]), np.var(data[:, 1]), np.var(data[:, 2]))
+        dim_index = variances.index(max(variances))
+        data_sorted = data[np.lexsort(data.T[dim_index, None])]
+
+        point = data_sorted[int(len(data)/2)]
+        root = KD_node(point)
+        root.left = createKDTree(root.left, data_sorted[: int((len(data) / 2))])  
+        root.right = createKDTree(root.right, data_sorted[int((len(data) / 2)):]) 
+        return root
+    init_root = KD_node(None)
+    root = createKDTree(init_root, points)
+
+    return parts
+
 
 
 
@@ -223,3 +249,4 @@ if __name__ == "__main__":
         coords, feats = test_iter.next()
         print("="*20, "check dataset", "="*20, 
             "\ncoords:\n", coords, "\nfeat:\n", feats)
+
